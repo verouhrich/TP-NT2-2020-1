480ea9abbf5252c4b2c0c829cee19f38cbecd744
Daniel Guzman <daniel.guzman@ort.edu.ar>
2020-04-30 02:51:42 -0300

    Definicion Proyecto

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bienvenidos NT2!</Text>
+      <Text>Proyecto a realizar: Cronometro Pomodoro.</Text>
     </View>
   );
 }
